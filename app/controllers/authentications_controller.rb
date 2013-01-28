7e50a70e67f75deec9760348ea4f2fcc1311efad
Leo Honkanen <leo.o.honkanen@gmail.com>
2013-01-28 19:26:38 +0200

    initial code dedupe

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -4,77 +4,71 @@ class AuthenticationsController < ApplicationController
     @authentications = current_user.authentications.all
   end
 
+  def logged_in(authentication)
+    flash[:notice] = "Logged in Successfully"
+    sign_in_and_redirect User.find(authentication.user_id)
+  end
+
+  def link(token, token_secret)
+    omni = request.env["omniauth.auth"]
+
+    current_user.authentications.create!(
+      provider: omni['provider'],
+      uid: omni['uid'],
+      token: token,
+      token_secret: token_secret
+    )
+
+    flash[:notice] = "Authentication successful."
+    sign_in_and_redirect current_user
+  end
+
+  def create
+    omni = request.env["omniauth.auth"]
+
+    user = User.new
+    user.email = omni['extra']['raw_info'].email
+
+    user.apply_omniauth(omni)
+
+    return user
+  end
 
   def facebook
     omni = request.env["omniauth.auth"]
-    authentication = Authentication.find_by_provider_and_uid(omni['provider'], omni['uid'])
+    authentication = Authentication.find_by_provider_and_uid(
+      omni['provider'], 
+      omni['uid'])
 
     if authentication
-      flash[:notice] = "Logged in Successfully"
-      sign_in_and_redirect User.find(authentication.user_id)
+      logged_in(authentication)
     elsif current_user
       token = omni["credentials"].token
       token_secret = ""
-
-      # Link account
-      current_user.authentications.create!(
-        provider: omni['provider'],
-        uid: omni['uid'],
-        token: token,
-        token_secret: token_secret
-      )
-
-      flash[:notice] = "Authentication successful."
-      sign_in_and_redirect current_user
+      link(token, token_secret)
     else
-      user = User.new
-      user.email = omni['extra']['raw_info'].email
-
-      user.apply_omniauth(omni)
-
-      if user.save
-        flash[:notice] = "Logged in."
-        sign_in_and_redirect User.find(user.id)
-      else
-        session[:omniauth] = omni.except('extra')
-        redirect_to new_user_registration_path
-      end
+      user = create
+      flash[:notice] = "Logged in."
+      sign_in_and_redirect User.find(user.id)
     end
   end
 
   def twitter
     omni = request.env["omniauth.auth"]
-    authentication = Authentication.find_by_provider_and_uid(omni["provider"], omni["uid"])
+    authentication = Authentication.find_by_provider_and_uid(
+      omni["provider"], 
+      omni["uid"])
     
     if authentication
-      flash[:notice] = "Logged in Successfully"
-      sign_in_and_redirect User.find(Authentication.user_id)
+      logged_in(authentication)
     elsif current_user
       token = omni["credentials"].token
       token_secret = omni["credentials"].secret
-      
-      current_user.authentications.create!(
-        provider: omni["provider"],
-        uid: omni["uid"],
-        token: token,
-        token_secret: token_secret
-      )
-      
-      flash[:notice] = "authentication successful."
-      sign_in_and_redirect current_user
+      link(token, token_secret)
     else 
-      user = User.new
-      user.email = omni["extra"]["raw_info"].email
-      
-      user.apply_omniauth(omni)
-      
-      if user.save
-        flash[:notice] = "Logged in"
-        sign_in_and_redirect User.find(user.id)
-      else
-        session[:omniauth] = omni.except("extra")
-        redirect_to new_user_registration_path
-      end
+      create
+      session[:omniauth] = omni.except('extra')
+      redirect_to new_user_registration_path
     end
   end
 
