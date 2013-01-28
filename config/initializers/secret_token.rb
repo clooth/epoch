7b2d3ed804c38b9dcbe58e71b183e36bc4bae60d
Nico Hämäläinen <zenverse@gmail.com>
2013-01-28 14:12:00 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epoch::Application.config.secret_token = '61a6b418d1974ab4a74f45c1b0409014ff0a7e57b6470dfd4743928757bd4a5ac3398d248b9369490a1b044615475e7f29a2e657be850805bacd6df73c18459f'
